bdd4708542eb4aa9d45d6e8e694fb945ebaba0ff
solidbird <serdar.akcay00@gmail.com>
2023-01-08 20:01:39 +0100

    Update traceMain

--- cfgtree.cpp
+++ cfgtree.cpp
@@ -11,14 +11,71 @@ using namespace std;
 //KNOB<string> KnobHelp(KNOB_MODE_WRITEONCE, "pintool", "h", NULL, NULL);
 ofstream OutFile;
 map<string, string> bblAddress;
-vector<string> pointToAddress;
+vector<string> nextInstructionAddress;
+
+
+/*static void traceCallIfBefore(){
+	cout << "!!!CALL IF BEFORE!!!" << endl;
+}*/
+
 
 VOID traceMain(TRACE trace, VOID* v){
 	stringstream insBuilder;
 	stringstream tmpBuilder;
 
+	ADDRINT bblhead = TRACE_Address(trace);
+	ADDRINT nextInstructionAddress = TRACE_Address(trace);
+	string cfIns = "";
 
+	IMG img = IMG_FindByAddress(TRACE_Address(trace));
+	RTN rtn = TRACE_Rtn(trace);
+
+	if(!IMG_Valid(img) || !(IMG_IsMainExecutable(img))) return;
+
+	//cout << TRACE_NumBbl(trace) << endl;
+	//TRACE_InsertCall(trace, IPOINT_BEFORE, (AFUNPTR)traceCallIfBefore, IARG_END);
+	//if (!TRACE_HasFallThrough(trace)) cout << "!!!FALL THROUGH!!!" << endl;
+
+	RTN_Open(rtn);
 	for( BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl) ){
+		
+		
+		cout << " |" << endl;
+		cout << " |" << endl;
+
+		if(bblhead == nextInstructionAddress){
+			cout << "true: " << cfIns << endl;
+
+		}else{
+			cout << "false: " << cfIns << endl; 
+		}
+		
+		cout << " |" << endl;
+		cout << " v" << endl;
+
+		bblhead = BBL_Address(bbl);
+		cout << "--- BBL Head Address: " << hex << bblhead << " ---" << endl;
+
+		cout << RTN_Name(rtn) << endl;
+		for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins) ){
+			cout << "\t" << " IP: " << hex << INS_Address(ins) << " IP+1: "  << INS_NextAddress(ins) << "\n\t\t << " << INS_Disassemble(ins) << endl;
+			if(INS_IsControlFlow(ins)){
+				if(!INS_IsIndirectControlFlow(ins)){
+
+				}
+				nextInstructionAddress = INS_NextAddress(ins);
+				cfIns = INS_Disassemble(ins);
+			}
+		} 
+
+
+	}
+	RTN_Close(rtn);
+
+
+
+
+	/*for( BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl) ){
 		cout << "*****" << endl;
 
 		ADDRINT headAddressBbl = BBL_Address(bbl);
@@ -31,14 +88,14 @@ VOID traceMain(TRACE trace, VOID* v){
 
 		string tmp = insBuilder.str();
 		int pos = tmp.rfind("0x")+2;
-		pointToAddress.push_back(tmp.substr(pos, pos+12));
+		nextInstructionAddress.push_back(tmp.substr(pos, pos+12));
 
 		tmpBuilder << hex << headAddressBbl;
 
 		bblAddress[tmpBuilder.str()] = insBuilder.str();
 		insBuilder.str(std::string());
 		tmpBuilder.str(std::string());
-	}
+	}*/
 }
 
 VOID mainIMG(IMG img, VOID* v){
@@ -63,9 +120,9 @@ VOID Fini(INT32 code, VOID *v){
 	//}
 	OutFile.setf(ios::showbase);
 
-	for(auto x : bblAddress /*pointToAddress*/){
-		OutFile << x.second << endl;
-		OutFile << "--" << endl;
+	for(auto x : bblAddress /*nextInstructionAddress*/){
+		//OutFile << x.second << endl;
+		//OutFile << "--" << endl;
 		//if(x.find(x.first) != 0){
 		//}
 	}
@@ -77,6 +134,7 @@ VOID Fini(INT32 code, VOID *v){
 int main(int argc, char* argv[]){
 
 	PIN_InitSymbols();
+	InitRegTables();
 	if(PIN_Init(argc,argv)) return Usage();
 
 	OutFile.open("data.out");
